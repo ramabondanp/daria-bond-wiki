b6856e0b716309df128a26f03c63f5707862d26e
Nima Fanniasl <smartnimaco@gmail.com>
2025-11-07 23:57:22 +0330

    Fixed spacing

--- scripts/get-updates.py
+++ scripts/get-updates.py
@@ -54,7 +54,6 @@ with open("scripts/unlisted_updates.json", "r") as f:
                             {update["description"]}
 
                             <h3>Download: <a href="{update["url"]}">{update["filename"]}</a></h3>
-                            
                             {f"<h3>Download Boot + Recovery Image: <a href=\"{update["boot_img"]}\">boot.img</a></h3>" if update.get("boot_img", False) else ""}
 
                             <p>File Size: {size_gb:.2f} GB - md5sum: {update["md5sum"]} - API Level {update["api_level"]} - Type: {update.get("updatetype", "N/A")}</p>
